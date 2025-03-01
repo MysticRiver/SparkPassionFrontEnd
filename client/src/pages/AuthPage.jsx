eec0cad48b3bb733f505c3c4a62a7e2dc1117b77
Tarik Carey <renewed@gmail.com>
2025-02-28 19:37:41 -0700

    Setup AuthPage.jsx

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {useState} from 'react'
 
 const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
-}
+    const [isLogin, setIsLogin] = useState(true);
 
-export default AuthPage
+  return (
+    <div 
+    className='min-h-screen flex items-center justify-center bg-gradient-to-br from-red-500 to-pink-500 p-4'>AuthPage
+    <div className='w-full max-w-md'>
+        <h2 className='text-center text-3xl font-extrabold text-white mb-8'>
+            {isLogin ? "Sign in to Spark Passion": "Create a Spark Passion Account"}
+
+        </h2>
+        <div className='bg-white p-8 shadow-xl rounded-lg'>
+            {isLogin ? <LoginForm /> : <SignupForm />}
+        </div>
+    </div>
+    </div>
+  )
+};
+
+export default AuthPage;
