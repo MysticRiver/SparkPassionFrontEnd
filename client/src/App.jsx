bfa4ba9de6fd8800c42411fa9b3318321fb1cad8
Tarik Carey <renewed@gmail.com>
2025-02-28 19:22:30 -0700

    Setup React Router and basic app/website layout
    Setup React Router and Basic app/Webpage layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage.jsx'
+import AuthPage from './pages/AuthPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500 font-bold">Hello World</h1>
-    </div>
-  )
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route path="/" element={< HomePage/>} />
+        <Route path="/auth" element={< AuthPage/>} />
+        <Route path="/profile" element={< ProfilePage/>} />
+        <Route path="/chat/:id" element={< ChatPage/>} />
+      </Routes>
+      </div>
+
+
+  
+  );
 }
 
-export default App
+export default App;
